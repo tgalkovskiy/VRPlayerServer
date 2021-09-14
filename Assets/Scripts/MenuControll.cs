cb0785ed81bdf406dde86702529e53c8752380a3
Timur <tgalkovskiy@gmail.com>
2021-09-14 18:38:07 +0300

    add Icon and TimeLine

--- Assets/Scripts/MenuControll.cs
+++ Assets/Scripts/MenuControll.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuControll : MonoBehaviour
 {
@@ -8,7 +9,13 @@ public class MenuControll : MonoBehaviour
     [SerializeField] private GameObject _chooseExpPanel = default;
     [SerializeField] private GameObject _chooseFavorit = default;
     [SerializeField] private GameObject _chooseEvent = default;
-
+    [SerializeField] private Image _videoIcon = default;
+    [SerializeField] private Text _videoDescription = default;
+    public static MenuControll Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
     public void ShowPanels(GameObject panel)
     {
         _chooseVideoPanel.SetActive(false);
@@ -16,5 +23,10 @@ public class MenuControll : MonoBehaviour
         _chooseFavorit.SetActive(false);
         _chooseEvent.SetActive(false);
         panel.SetActive(true);
+    }
+    public void ChangeVideoPanel(Sprite sprite, string description)
+    {
+        _videoIcon.sprite = sprite;
+        _videoDescription.text = description;
     }
 }
--- Assets/Scripts/VideoCell.cs
+++ Assets/Scripts/VideoCell.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 public class VideoCell : MonoBehaviour
 {
    [SerializeField]private Image _image = default;
+   [SerializeField]private Text _descriptionText;
    [SerializeField]private int number = default;
-   [SerializeField]private string discription = default;
-   
-    public void SetParamertsCell(Sprite sprite, int number, string discription)
+   [SerializeField]private string _description = default;
+
+    public void SetParamertsCell(Sprite sprite, int number, string _description)
     {
         _image.sprite = sprite;
         this.number = number;
-        this.discription = discription;
+        this._description = _description;
+        this._descriptionText.text = _description;
     }
     public void SendNumberVideo()
     {
         LobyManager.Instance.CommandVideo(number);
+        MenuControll.Instance.ChangeVideoPanel(_image.sprite, _description);
     }
 }
